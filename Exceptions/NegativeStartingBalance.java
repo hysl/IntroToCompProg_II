b9b36dfb505b99935c4ae581c13bf546b7dffb9e
hysl <47994191+hysl@users.noreply.github.com>
2019-09-12 19:04:32 -0400

    Create NegativeStartingBalance.java

---
+++ Exceptions/NegativeStartingBalance.java
@@ -0,0 +1,17 @@
+/*
+Helen Li (1260857)
+April 1, 2019
+*/
+
+public class NegativeStartingBalance extends Exception
+{
+	public NegativeStartingBalance()
+	{
+		super("Error: Negative starting balance");
+	}
+
+	public NegativeStartingBalance(double amount)
+	{
+		super("Error: Negative starting balance: " + amount);
+	}
+}
