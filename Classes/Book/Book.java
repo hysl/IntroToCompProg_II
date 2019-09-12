976988e5c8f95470d3b145b6adbdee72a299c760
hysl <47994191+hysl@users.noreply.github.com>
2019-09-12 18:42:20 -0400

    Create Book.java

---
+++ Classes/Book/Book.java
@@ -0,0 +1,16 @@
+/*
+Helen Li
+February 4, 2019
+*/
+
+public class Book
+{
+	String title;
+	String author;
+
+	public Book(String title, String author)
+	{
+		this.title = title;
+		this.author = author;
+	}
+}
