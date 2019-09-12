4ab496d0b90c9edec86f792090f7c999f02fd90c
hysl <47994191+hysl@users.noreply.github.com>
2019-09-12 19:03:59 -0400

    Create NegativeDeposit.java

---
+++ Exceptions/NegativeDeposit.java
@@ -0,0 +1,17 @@
+/*
+Helen Li (1260857)
+April 1, 2019
+*/
+
+public class NegativeDeposit extends Exception
+{
+	public NegativeDeposit()
+	{
+		super("Error: Negative value passed to the deposit method");
+	}
+
+	public NegativeDeposit(double amount)
+	{
+		super("Error: Negative value passed to the deposit method: " + amount);
+	}
+}
