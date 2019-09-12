169d47c37348d573c8b585301fd322c0ca3163bb
hysl <47994191+hysl@users.noreply.github.com>
2019-09-12 18:53:06 -0400

    Create Student.java

---
+++ Classes/Student/Student.java
@@ -0,0 +1,18 @@
+/*
+Helen Li
+February 4, 2019
+*/
+
+public class Student
+{
+	String name;
+	int id;
+	String dept;
+
+	public Student(String name, int id, String dept)
+	{
+		this.name = name;
+		this.id = id;
+		this.dept = dept;
+	}
+}
