0a420fbe2abae8a4480a36da4d6dc4c62127ee65
hysl <47994191+hysl@users.noreply.github.com>
2019-09-13 01:16:59 -0400

    Create Fib.java

---
+++ Recursion/Fib.java
@@ -0,0 +1,36 @@
+/*
+Helen Li
+May 6, 2019
+*/
+
+public class Fib
+{
+	public static void main(String[] args)
+	{
+		for(int i = 0; i <= 10; i++)
+			System.out.println(fib(i));
+	}
+
+	public static int fib(int n)
+	{
+		int num1 = 0;
+		int num2 = 1;
+		int num3 = 0;
+
+		if(n == 0)
+			return 0;
+		if(n == 1)
+			return 1;
+		else
+		{
+			for(int i = 2; i <= n; i++)
+			{
+				num3 = num1 + num2;
+				num1 = num2;
+				num2 = num3;
+			}
+		}
+
+		return num3;
+	}
+}
